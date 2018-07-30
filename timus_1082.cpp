0c8c6d7d12cf7d70e41fcd687afcc4d1cd977635
Saiful Islam Rasel <s.i.r.computerengineer24@gmail.com>
2018-07-30 14:26:03 +0600

    timus 1082 Gaby Ivanushka

---
+++ timus_1082.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+        printf("%d ",i);
+    printf("\n");
+    return 0;
+}
