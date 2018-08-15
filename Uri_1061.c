47c68f5f16cfc2e4e0279eb1eea42a5be6560d9f
Saiful Islam Rasel <s.i.r.computerengineer24@gmail.com>
2018-08-15 11:49:08 +0600

    "Uri 1061 modified"

--- Uri_1061.c
+++ Uri_1061.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 int main(){
-    int a,b,c,d,a2,b2,c2,d2,day,hour,min,sec;
-    printf("Dia ");
-    scanf("%d",&a);
-    scanf("%d : %d : %d",&b,&c,&d);
-    printf("Dia ");
-    scanf("%d",&a2);
-    scanf("%d : %d : %d",&b2,&c2,&d2);
-
+    char a[4];
+    int d,h,m,s,start,end,temp;
+    scanf("%s%d",a,&d);
+    scanf("%d : %d : %d",&h,&m,&s);
+    start=s+m*60+h*60*60+d*24*60*60;
+    scanf("%s%d",a,&d);
+    scanf("%d : %d : %d",&h,&m,&s);
+    end=s+m*60+h*60*60+d*24*60*60;
+    temp=end-start;
+    d = temp / 86400;
+    temp = temp % 86400;
+    h = temp / 3600;
+    temp = temp % 3600;
+    m = temp / 60;
+    s = temp % 60;
+    printf("%d dia(s)\n%d hora(s)\n%d minuto(s)\n%d segundo(s)\n",d,h,m,s);
     return 0;
 }
