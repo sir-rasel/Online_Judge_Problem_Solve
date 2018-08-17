b2697660c7ad26ab87802c71da9f106fa37a23f2
Saiful Islam Rasel <s.i.r.computerengineer.24@gmail.com>
2018-08-17 12:34:31 +0600

    timus 1044 lucky tickets

---
+++ timus_1044.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n, i, num, upto, sum, cnt, *arr;
+
+    cin >> n;
+
+    arr = new int [n/2 * 9 + 1];
+
+    for(i = 0; i < n/2 * 9 + 1; i++) arr[i] = 0;    // clearing garvage values.
+
+    upto = pow(10, n/2) - 1;
+
+    for(i = 0; i <= upto; i++) {
+        sum = 0;
+        num = i;
+        while(num) {
+            sum += num % 10;
+            num /= 10;
+        }
+        arr[sum]++;
+    }
+
+    cnt = 0;
+    for(i = 0; i <= n/2 * 9; i++) {
+        cnt += arr[i] * arr[i];
+    }
+
+    cout << cnt << '\n';
+
+    delete [] arr;
+
+    return 0;
+}
+
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//
+//int main()
+//{
+//    int n, i, num, upto, sum, cnt, *arr;
+//
+//    cin >> n;
+//
+//    arr = new int [n/2 * 9 + 1];
+//
+//    for(i = 0; i < n/2 * 9 + 1; i++) arr[i] = 0;    // clearing garvage values.
+//
+//    upto = pow(10, n/2) - 1;
+//
+//    for(i = 0; i <= upto; i++) {
+//        sum = 0;
+//        num = i;
+//        while(num) {
+//            sum += num % 10;
+//            num /= 10;
+//        }
+//        arr[sum]++;
+//    }
+//
+//    cnt = 0;
+//    for(i = 0; i <= upto; i++) {
+//        sum = 0;
+//        num = i;
+//        while(num) {
+//            sum += num % 10;
+//            num /= 10;
+//        }
+//
+//        cnt += arr[sum];
+//    }
+//
+//    cout << cnt << '\n';
+//
+//    delete [] arr;
+//
+//    return 0;
+//}
