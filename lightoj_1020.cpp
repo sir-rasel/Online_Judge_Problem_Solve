c3fafce01c98819dbef2170d37f5e26bd9a00bd0
Saiful Islam Rasel <s.i.r.computerengineer.24@gmail.com>
2018-08-23 21:26:41 +0600

    lightoj 1020 childwood game

---
+++ lightoj_1020.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int test;
+    scanf("%d",&test);
+    for(int cs=1;cs<=test;cs++){
+        long int num;
+        char name[10];
+        scanf("%ld %s",&num,name);
+        printf("Case %d: ",cs);
+        if(strcmp(name,"Alice")==0){
+            if(num%3==1) printf("Bob\n");
+            else printf("Alice\n");
+        }
+        else {
+            if(num%3==0) printf("Alice\n");
+            else printf("Bob\n");
+        }
+    }
+    return 0;
+}
