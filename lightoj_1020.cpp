cba619353ed18197c6a303b795f5f304ccc86ad8
Saiful Islam Rasel <s.i.r.computerengineer.24@gmail.com>
2018-08-29 23:14:09 +0600

    indentation maintain

--- lightoj_1020.cpp
+++ lightoj_1020.cpp
@@ -5,10 +5,12 @@ int main(){
     int test;
     scanf("%d",&test);
     for(int cs=1;cs<=test;cs++){
+
         long int num;
         char name[10];
         scanf("%ld %s",&num,name);
         printf("Case %d: ",cs);
+
         if(strcmp(name,"Alice")==0){
             if(num%3==1) printf("Bob\n");
             else printf("Alice\n");
@@ -17,6 +19,7 @@ int main(){
             if(num%3==0) printf("Alice\n");
             else printf("Bob\n");
         }
+
     }
     return 0;
 }
