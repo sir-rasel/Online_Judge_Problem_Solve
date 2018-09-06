e709f41495dcb33f7603fc003a5e9cc0a78baab4
Rasel <raselcse97@gmail.com>
2018-09-06 12:05:49 +0600

    codeforces 1028B unnatural condition

---
+++ codeforces_1028B.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;
+    int len = 400;
+    scanf("%d %d",&n,&m);
+    for(int i=0;i<400;i++) printf("5");
+    printf("\n");
+    for(int i=0;i<399;i++) printf("4");
+    printf("5\n");
+    return 0;
+}
