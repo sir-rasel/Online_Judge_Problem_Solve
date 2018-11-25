c70b300937a9fb561ece6ce8986793868ad91004
Saiful Islam Rasel <s.i.r.computerengineer.24@gmail.com>
2018-11-25 13:45:29 +0600

    timus 1563 Bayan

--- timus_1563.cpp
+++ timus_1563.cpp
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main(){
-    int test;
-    string a;
-    map <string,int> m;
-    cin>>test;
+    int n;
+    cin>>n;
     getchar();
-    while(test--){
-        getline(cin,a);
-        m[a]++;
+    map<string,int>repeat;
+    int counter=0;
+    while(n--){
+        string brand;
+        getline(cin,brand);
+        if(repeat[brand]==0) repeat[brand]++;
+        else counter++;
     }
-    int large=0;
-    for(int i=0;i<m.size();i++){
-        if(large<m[i]) large=m[i];
-    }
-    cout<<large<<endl;
+    cout<<counter<<endl;
     return 0;
 }
