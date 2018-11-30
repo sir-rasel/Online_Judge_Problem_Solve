bfb34394b351b121903246e8d18daeb2aa650740
Rasel <raselcse97@gmail.com>
2018-11-30 18:01:39 +0600

    timus 2031 overturned numbers

---
+++ timus_2031.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if (n == 1) printf("01\n");
+    else if (n == 2) printf("11 01\n");
+    else if (n == 3) printf("16 06 68\n");
+    else if (n == 4) printf("16 06 68 88\n");
+    else printf("Glupenky Pierre\n");
+    return 0;
+}
