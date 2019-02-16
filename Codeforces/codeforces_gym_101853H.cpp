e29883c1b4ea10c77894eaa2e4de83dccbf515ee
Saiful Islam Rasel <raselcse97@gmail.com>
2019-02-16 23:11:12 +0600

    codeforces gym 101853H Cube

---
+++ Codeforces/codeforces_gym_101853H.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, n, surf;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        surf = sqrt(n/6);
+        cout << surf << endl;
+    }
+    return 0;
+}
