fc7e7fe8d1136329a77f1a38d4980519a78343f6
Rasel <raselcse97@gmail.com>
2019-03-07 17:02:05 +0600

    codechef CHDIGER not finished yet

--- Codechef/codechef_CHDIGER.cpp
+++ Codechef/codechef_CHDIGER.cpp
@@ -2,17 +2,13 @@
 using namespace std;
 
 typedef unsigned long long int ull;
-deque<char>number;
+map<int,int>position;
+string s,c;
 
-ull valueCount(){
+ull valueCount(string temp){
     ull ans = 0;
-    for(int i=0;i<number.size();i++) ans = (ans*10)+(number[i]-48);
-    return ans;
-}
-
-ull valueCountFirst(string s){
-    ull ans = 0;
-    for(int i=0;i<s.size();i++) ans = (ans*10)+(s[i]-48);
+    for(int i=0;i<temp.size();i++)
+        if(!position[i]) ans = (ans*10)+(temp[i]-48);
     return ans;
 }
 
@@ -21,21 +17,22 @@ int main(){
     scanf("%d",&test);
     getchar();
     while(test--){
-        number.clear();
-        string s;
-        char c;
+        position.clear();
         cin>>s>>c;
 
-        ull res=valueCountFirst(s);
-        sort(s.begin(),s.end(),less<char>());
-        int totalDigit=s.size();
-        for(int i=0;i<totalDigit;i++) number.push_back(s[i]);
+        ull res=valueCount();
 
-        int start=0;
-        while(totalDigit--){
-            number.pop_front();
-            number.push_back(c);
-            res = min(res,valueCount());
+        int totalDigit=s.size();
+        for(int i=0;i<totalDigit;i++){
+            string temp=s;
+            temp+=c;
+            position[i]++;
+            ull tempRes = valueCount(temp);
+            if(tempRes<=res) {
+                res=tempRes;
+                s+=c;
+            }
+            else position[i]=0;
         }
         printf("%llu\n",res);
     }
