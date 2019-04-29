0009a6c951bfccf6aedb1222efbf08e6c8e55a14
Saiful Islam Rasel <raselcse97@gmail.com>
2019-04-29 23:34:03 +0600

    toph/DynamicProgramming Easy sequence 2

---
+++ toph/DynamicProgramming/easy-sequence-2.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long int ull;
+
+map<ull,ull>dp;
+
+ull f(ull x){
+    if(x==1) return 1;
+    if(x==2) return 2;
+    if(x==3) return 4;
+    if(dp.count(x)!=0) return dp[x];
+    if(x%2==0) return dp[x]=f(x/2)*f(x/2)+1;
+    else return dp[x]=f(x/2)*f(x/2+1)+2;
+}
+
+int main(){
+    int test,cs=1;
+    scanf("%d",&test);
+    while(test--){
+        ull x;
+        scanf("%llu",&x);
+        printf("Case %d: %llu\n",cs++,f(x));
+    }
+    return 0;
+}
