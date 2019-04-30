1fc02cdf65c2b1b54813d1b9c4b0f4b45cea553c
Saiful Islam Rasel <raselcse97@gmail.com>
2019-04-30 18:45:59 +0600

    toph/tree How many ways!

---
+++ toph/tree/how-many-ways.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int test,cs=1;
+	scanf("%d",&test);
+	while(test--){
+		int m,n;
+		scanf("%d %d",&m,&n);
+		for(int i=0;i<m;i++) {
+			int a,b;
+			scanf("%d %d",&a,&b);
+		}
+		printf("Case %d: 1\n",cs++);
+	}
+	return 0;
+}
