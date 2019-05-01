0e86ae69dd71303157fdad5f8c49e31a9784b789
Saiful Islam Rasel <raselcse97@gmail.com>
2019-05-01 23:40:46 +0600

    toph/implementation Sofdor Alis Magical Black Board

---
+++ toph/implementation/sofdor-alis-magical-black-board.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main(){
+    int test;
+    scanf("%d",&test);
+    while(test--){
+        ll a,b;
+        scanf("%lld %lld",&a,&b);
+        printf("%lld\n",a*b);
+    }
+    return 0;
+}
