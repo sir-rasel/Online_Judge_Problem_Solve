b8dc88879b7473b1147ad5863815a6c60e8a4030
Saiful Islam Rasel <raselcse97@gmail.com>
2019-05-02 13:33:42 +0600

    toph/geomatry Pakna and his polygon

---
+++ toph/geomatry/pakna-and-his-polygon.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int test;
+    scanf("%d",&test);
+    while(test--){
+        double a;
+        scanf("%lf",&a);
+        printf("%.5lf\n",3.0*sqrt(3.0)/2.0*a*a);
+    }
+    return 0;
+}
