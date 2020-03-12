7cbf183fae17ea4bd569622a2ad80cc86eb1bc68
Saiful Islam Rasel <raselcse97@gmail.com>
2020-03-12 23:39:43 +0600

    leetcode 278. First Bad version

---
+++ letcode/278_first_bad_version.cpp
@@ -0,0 +1,18 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1,high = n,ans=-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(isBadVersion(mid)){
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return ans;
+    }
+};
