e4b4246dd9274de73001f6adaa7dd560a66d9969
Saiful Islam Rasel <raselcse97@gmail.com>
2020-03-17 15:16:27 +0600

    codeforces 1325A EhAb And gCd

---
+++ Codeforces/codeforces_1325A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main(){
+    int test;
+    scanf("%d",&test);
+    while(test--){
+        int n;
+        scanf("%d",&n);
+        printf("%d %d\n",1,n-1);
+    }
+    return 0;
+}
