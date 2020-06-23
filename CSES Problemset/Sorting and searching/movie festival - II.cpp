cf3d45bf47ab071ac56df50fb2630f5228ba6602
Saiful Islam Rasel <raselcse97@gmail.com>
2020-06-23 11:57:13 +0600

    movie festival - II added from sorting and searching section

---
+++ CSES Problemset/Sorting and searching/movie festival - II.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+
+int main(){
+    int n, k;
+    scanf("%d %d",&n,&k);
+    vector<pii> v;
+
+    for(int i=0;i<n;i++) {
+        int x, y;
+        scanf("%d %d",&x,&y);
+        v.push_back({y, x});
+    }
+    sort (v.begin(), v.end());
+
+    int timing[k] = {0};
+    int cnt = 0;
+    multiset <int> p;
+
+    for(int i=0;i<k;i++) p.insert(0);
+
+    for (auto i:v) {
+        auto it = p.upper_bound(i.second);
+        if (it == p.begin())
+            continue;
+
+        it--;
+        cnt++;
+
+        p.erase(it);
+        p.insert(i.first);
+    }
+
+    printf("%d\n",cnt);
+
+    return 0;
+}
