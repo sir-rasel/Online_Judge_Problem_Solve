32a28d6b24ed4861007d8684217e85d06c288c8c
Saiful Islam Rasel <raselcse97@gmail.com>
2020-08-20 15:18:54 +0600

    leetcode 143. Reorder list

---
+++ leetcode/143_reorder_list.py
@@ -0,0 +1,27 @@
+class Solution:
+    def reorderList(self, head):
+        if not head: 
+            return
+        
+         #step 1: find middle
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        #step 2: reverse second half
+        prev, curr = None, slow.next
+        while curr:
+            nextt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextt    
+        slow.next = None
+        
+        #step 3: merge lists
+        head1, head2 = head, prev
+        while head2:
+            nextt = head1.next
+            head1.next = head2
+            head1 = head2
+            head2 = nextt
